943fc48f4d2657111d9e07e59cfc35d3832b186a
MarSury03 <sm.torres@alum.up.edu.pe>
2026-05-28 10:51:48 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Proyecto BI - Siniestralidad vial en el Perú
+
+Este archivo se utilizará posteriormente para scripts de limpieza,
+transformación o carga de datos.
+"""
+
+def main():
+    print("Proyecto BI - Siniestralidad vial en el Perú")
+
+if __name__ == "__main__":
+    main()
